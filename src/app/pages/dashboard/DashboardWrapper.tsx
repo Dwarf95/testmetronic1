666f9e60b71925203d708f41bf2ab292e485d69a
devlazar <lazar.stankovic@atessoft.rs>
2022-02-03 22:09:10 +0100

    first commit test

---
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, {FC} from 'react'
+import {useIntl} from 'react-intl'
+import {PageTitle} from '../../../_metronic/layout/core'
+
+const DashboardPage: FC = () => (
+  <>
+    <div className='w-full'>
+      <p className='text-primary text-center fs-4'>Dashboard content goes here</p>
+    </div>
+  </>
+)
+
+const DashboardWrapper: FC = () => {
+  const intl = useIntl()
+  return (
+    <>
+      <PageTitle breadcrumbs={[]}>{intl.formatMessage({id: 'MENU.DASHBOARD'})}</PageTitle>
+      <DashboardPage />
+    </>
+  )
+}
+
+export {DashboardWrapper}
---
+++ src/app/routing/Routes.tsx
@@ -0,0 +1,50 @@
+/**
+ * High level router.
+ *
+ * Note: It's recommended to compose related routes in internal router
+ * components (e.g: `src/app/modules/Auth/pages/AuthPage`, `src/app/BasePage`).
+ */
+
+import React, {FC} from 'react'
+import {Redirect, Switch, Route} from 'react-router-dom'
+import {shallowEqual, useSelector} from 'react-redux'
+import {MasterLayout} from '../../_metronic/layout/MasterLayout'
+import {PrivateRoutes} from './PrivateRoutes'
+import {Logout, AuthPage} from '../modules/auth'
+import {ErrorsPage} from '../modules/errors/ErrorsPage'
+import {RootState} from '../../setup'
+import {MasterInit} from '../../_metronic/layout/MasterInit'
+
+const Routes: FC = () => {
+  const isAuthorized = useSelector<RootState>(({auth}) => auth.user, shallowEqual)
+
+  return (
+    <>
+      <Switch>
+        {!isAuthorized ? (
+          <Route>
+            <AuthPage />
+          </Route>
+        ) : (
+          <Redirect from='/auth' to='/' />
+        )}
+
+        <Route path='/error' component={ErrorsPage} />
+        <Route path='/logout' component={Logout} />
+
+        {!isAuthorized ? (
+          <Redirect to='/auth/login' />
+        ) : (
+          <>
+            <MasterLayout>
+              <PrivateRoutes />
+            </MasterLayout>
+          </>
+        )}
+      </Switch>
+      <MasterInit />
+    </>
+  )
+}
+
+export {Routes}
